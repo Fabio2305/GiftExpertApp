c544840e43357d2ed15b7d282bfae51cda4821b9
Fabio Barriga <fasabsg@gmail.com>
2021-01-06 11:14:56 -0500

    First commit

---
+++ src/helpers/getGif.js
@@ -0,0 +1,17 @@
+export const getGif = async (category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=KX8oTfzuR8kTqVhHh3yFSmnPi4YbiBDW`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(gif => {
+        return {
+            id: gif.id,
+            title: gif.title,
+            url: gif.images?.downsized_medium.url
+        }
+    })
+    console.log(gifs);
+
+    return gifs;
+}
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+import { getGif } from "../helpers/getGif"
+
+
+export const useFetchGifs = (category) => {
+
+    const [state, setState] = useState({
+        data: [],
+        loading: true,
+    })
+
+    useEffect(() => {
+        
+        getGif(category)
+            .then(imgs =>{
+             
+                    
+                    console.log(imgs)
+                    setState({
+                        data: imgs,
+                        loading: false,
+                    })
+            })
+
+    }, [category])
+
+    return state;
+
+}
